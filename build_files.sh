0f85e2012b27df7de68056a571a828857faff4a2
KILUMU VENKATA LAXMI <kvlakshmi2358@gmail.com>
2025-06-06 19:18:04 +0530

    e

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Python setup
-/usr/bin/python3.9 -m ensurepip --upgrade
-/usr/bin/python3.9 -m pip install --upgrade pip
-/usr/bin/python3.9 -m pip install -r requirements.txt
+python -m ensurepip --upgrade
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 
 # Django setup
-python3.9 manage.py collectstatic --noinput
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py makemigrations
+python manage.py migrate
 
 # Create public directory and move static files into it
 mkdir -p public/static
